bacc9fb84630fe15181957f33e3f9d7f7dafccb8
Yohannes T. <lightshadow011@gmail.com>
2024-07-10 10:51:52 +0300

    reactive netflux initial commit

---
+++ src/test/java/com/yigbu/reactive_netflux/ReactiveNetfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yigbu.reactive_netflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveNetfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
